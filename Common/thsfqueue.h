e3b5bba843ad5d63aa87b857702c44334c1f52db
Hanzala Bhutto <hbhutto@trafix.com>
2024-06-20 17:17:45 +0500

    thread safe queue

---
+++ Common/thsfqueue.h
@@ -0,0 +1,83 @@
+#pragma once
+#include "common.h"
+
+namespace clsrv
+{
+	namespace net
+	{
+		template<typename T>
+		class thsfqueue
+		{
+		public:
+			thsfqueue() = default;
+			thsfqueue(const thsfqueue<T>&) = delete;
+
+			virtual ~thsfqueue()
+			{
+				clear();
+			}
+
+			const T& front()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.front();
+			}
+
+			const T& back()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.back();
+			}
+
+			void push_back(const T& item)
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.emplace_back(std::move(item));
+			}
+
+			void push_front(const T& item)
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.emplace_front(std::move(item));
+			}
+
+			bool empty()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.empty();
+			}
+
+			size_t count()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.size();
+			}
+
+			void clear()
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.clear();
+			}
+
+			T pop_front()
+			{
+				std::scoped_lock lock(muxQueue);
+				auto t = std::move(deqQueue.front());
+				deqQueue.pop_front();
+				return t;
+			}
+
+			T pop_back()
+			{
+				std::scoped_lock lock(muxQueue);
+				auto t = std::move(deqQueue.back());
+				deqQueue.pop_back();
+				return t;
+			}
+
+		protected:
+			std::mutex muxQueue;
+			std::deque<T> deqQueue;
+		};
+	}
+}
